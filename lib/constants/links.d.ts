d7763e56068072d3585a50f14699e060f8b481f1
Sangbin Lee <leesangbin@outlook.com>
2023-07-29 16:13:30 +0900

    fix packages

--- lib/constants/links.d.ts
+++
@@ -1,3 +0,0 @@
-export declare const MAPLESTORY_HOME = "https://maplestory.nexon.com";
-export declare const MAPLESTORY_RANKING_SEARCH: string;
-export declare const MAPLESTORY_CHARACTER_DETAIL = "https://maplestory.nexon.com/Common/Character/Detail";
